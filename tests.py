4aa3e888f2de784a626923ccd554e51650a9e762
Husanboy Usmonov <husanghost@gmail.com>
2023-04-06 12:00:53 +0200

    completed tests about request response and time

--- tests.py
+++ tests.py
@@ -1,7 +1,13 @@
 from unittest import TestCase
 from model import ChatGPTModel
-
+import requests
 
 class TestRequest(TestCase):
     def test_request_get(self):
-        self.assertEqual()
+        req=requests.get('https://openai80.p.rapidapi.com/chat/completions')
+        self.assertNotEqual(req.status_code,'204')
+        
+    def test_correct_answer(self):
+        answer1=ChatGPTModel('Capital of Poland')
+        answer2=ChatGPTModel('Capital of Poland')
+        self.assertEqual(answer1.main(), answer2.main())
