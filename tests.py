de671171dc671291731b62a291e92e5211fe5e4d
Husanboy Usmonov <husanghost@gmail.com>
2023-04-07 22:37:32 +0200

    added latest tests and integrated with Telgram and hido the token

--- tests.py
+++ tests.py
@@ -1,13 +1,25 @@
 from unittest import TestCase
-from model import ChatGPTModel
+from model import ChatGPTModel,NotValidInputError
 import requests
+from bot import TOKEN
+
 
 class TestRequest(TestCase):
+    def setUp(self):
+        model=ChatGPTModel()
+
     def test_request_get(self):
         req=requests.get('https://openai80.p.rapidapi.com/chat/completions')
         self.assertNotEqual(req.status_code,'204')
         
     def test_correct_answer(self):
-        answer1=ChatGPTModel('Capital of Poland')
-        answer2=ChatGPTModel('Capital of Poland')
-        self.assertEqual(answer1.main(), answer2.main())
+        answer1=self.model('Capital of Poland')
+        self.assertEqual(answer1.main(), 'Warsaw')
+
+    def test_user_input(self):
+        with self.assertRaises(NotValidInputError):
+            resp=ChatGPTModel(' ').main()
+
+
+    def test_bot_token(self):
+        self.assertIsNone(TOKEN,'')
